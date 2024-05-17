b7f25826f826b124681ba7779ac9125fbf3d2608
Rashad <2018rashad@gmail.com>
2024-05-18 00:25:06 +0300

    Last changes from phase 1

---
+++ Pages/Staff.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BloodDonation.Pages
+{
+    public class StaffModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
